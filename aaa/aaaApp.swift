6dc196770f80624d11e32fda15e4a75010649b40
Andrii Padalka <andrey.padalka@gmail.com>
2021-07-22 12:47:11 +0200

    Initial Commit

---
+++ aaa/aaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aaaApp.swift
+//  aaa
+//
+//  Created by AndreyP on 22/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct aaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
